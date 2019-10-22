c6386f944faae5bb2d184c2892d12d4c03435e85
Jesse Dunlap <jesse@hydrais.com>
2019-10-21 22:36:16 -0500

    Adamite API auto-generates a key now if one doesn't exist

--- src/Adamite.js
+++ src/Adamite.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const low = require("lowdb");
+const chalk = require("chalk");
 const FileSync = require("lowdb/adapters/FileSync");
 const CommandLineInterface = require("./cli");
 const ApiInterface = require("./api");
@@ -9,17 +10,24 @@ const ServiceRegistry = require("./registry/ServiceRegistry");
 class Adamite {
   constructor(config) {
     this.config = config;
-    this.initializeDatabase();
     this.keys = new KeyRegistry(this);
     this.services = new ServiceRegistry(this);
-    // this.cli = new CommandLineInterface(this);
     this.api = new ApiInterface(this);
+    this.initializeDatabase();
   }
 
   initializeDatabase() {
     if (!fs.existsSync("data")) fs.mkdirSync("data");
+    const apiJsonExists = fs.existsSync("data/api.json");
+
     this.db = low(new FileSync("data/api.json"));
     this.db.defaults({ keys: [] }).write();
+
+    if (!apiJsonExists) {
+      this.log("api", `👋   ${chalk.green("a new API key has been generated and can be viewed in data/api.json")}`);
+
+      this.keys.addKey(); // add a default API key
+    }
   }
 
   log(component, message) {
